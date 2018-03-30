0ec4f016a527cc3ca67724d6de37b985f89df592
jeretome <jeremias.tome@live.com.ar>
2018-03-30 12:52:16 -0300

    UI: Creado Home en Ionic

---
+++ mascotapp-frontend/src/pages/categories/categories.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-categories',
+  templateUrl: 'categories.html'
+})
+export class CategoriesPage {
+  selectedItem: any;
+  items: Array<{title: string, description: string, icon: string}>;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    // If we navigated to this page, we will have an item available as a nav param
+    this.selectedItem = navParams.get('item');
+
+    this.items = [];
+    this.items.push({
+      title: 'Perdidos ',
+      description: 'Aquí podrás buscar aminales perdidos o encontrados',
+      icon: 'paw'
+    });
+    this.items.push({
+      title: 'Adopciones ',
+      description: 'Aquí podrás buscar mascotas puestas en adopción',
+      icon: 'paw'
+    });
+  }
+
+  itemTapped(event, item) {
+    // That's right, we're pushing to ourselves!
+    this.navCtrl.push(CategoriesPage, {
+      item: item
+    });
+  }
+}
