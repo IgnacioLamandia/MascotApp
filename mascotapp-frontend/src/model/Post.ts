3e2ff6a1a2a9f72805a31ec8142986341f88f9ea
jeretome <jeremias.tome@live.com.ar>
2018-04-02 11:56:30 -0300

    UI: Agregado clase PostProvider:  - Método getAll que le pega al controller rest

---
+++ mascotapp-frontend/src/model/Post.ts
@@ -0,0 +1,33 @@
+export class Post {
+
+	id: number;
+  description: string;
+  image: string;
+  longitude: number;
+  latitude: number;
+  address: string;
+  category: Category;
+
+	constructor(
+  	descripcion: string,
+  	imagen: string,
+  	latitude: number,
+  	longitude: number,
+  	address: string,
+  	category: Category,
+  	id?:number) {
+  	this.id = id;
+		this.description = descripcion;
+		this.image = imagen;
+		this.longitude = longitude;
+		this.latitude = latitude;
+		this.address = address;
+		this.category = category;
+	}
+}
+
+export enum Category {
+  ENCONTRADO = "Encontrado",
+	PERDIDO = "Perdido",
+	ADOPCION = "En Adopcion"
+}
---
+++ mascotapp-frontend/src/providers/posts/post.ts
@@ -0,0 +1,29 @@
+import { HttpClient ,HttpHeaders} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Post } from '../../model/Post';
+import { Http, Response } from "@angular/http";
+import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/catch';
+import { Observable } from "rxjs/Observable";
+
+
+@Injectable()
+export class PostProvider {
+  private apiUrl = 'http://127.0.0.1:9000/';
+  constructor(private http: Http) { }
+
+  findAll(): Observable<Post[]>  {
+     return this.http.get(this.apiUrl+"publicaciones")
+      .map(res => {
+        return res.json().map(post => {
+        	console.log(post);
+          return new Post(
+              post.description,
+              "data:image/jpg;base64," + post.image,
+              post.latitude, post.longitude,
+              post.address, post.category, post.id
+          );
+        });
+      });
+  }
+}
