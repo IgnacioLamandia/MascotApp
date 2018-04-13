d5f3ae15b89e6ebc3b7d0852407e19765bb16763
ignaciolamandia <ignaciolamandia@gmail.com>
2018-04-13 19:33:01 -0300

    agregadas algunas descripciones mas reales

--- mascotapp-backend/src/main/java/mascotapp/service/HibernateDataService.java
+++ mascotapp-backend/src/main/java/mascotapp/service/HibernateDataService.java
@@ -46,10 +46,12 @@ public class HibernateDataService {
 		Post post3 = new Post("Gato busca familia", Encoder.encode(cats.get(2), "jpg"),0,0,"Solano",Category.ADOPCION);
 
 		//perros
-		Post post4 = new Post("Perro busca familia", Encoder.encode(dogs.get(0), "jpg"),0,0,"Calle 5 y 159",Category.ADOPCION);
-		Post post5 = new Post("Perro encontrado", Encoder.encode(dogs.get(1), "jpg"),0,0,"Calle 5 y 159",Category.ENCONTRADO);
-		Post post6 = new Post("Perro perdido", Encoder.encode(dogs.get(2), "jpg"),0,0,"Calle 5 y 159",Category.PERDIDO);
-		Post post7 = new Post("Perro callejero busca familia",Encoder.encode(dogs.get(3), "jpg"),0,0,"Calle 5 y 159",Category.ADOPCION);
+		Post post4 = new Post("Guido es increíble, ya esta super listo para dejar el refu!! se entrega castrado, desparasitado y con contrato de adopción. Es tamaño chico.\n" + 
+				"Veni a conocer al bombón de GUIDO en nuestra próxima jornada de visita!", Encoder.encode(dogs.get(0), "jpg"),0,0,"Bernal",Category.ADOPCION);
+		Post post5 = new Post("Fue encontrado en el centro de quilmes con collar gris", Encoder.encode(dogs.get(1), "jpg"),0,0,"Quilmes",Category.ENCONTRADO);
+		Post post6 = new Post("Perro perdido", Encoder.encode(dogs.get(2), "jpg"),0,0,"Berazategui",Category.PERDIDO);
+		Post post7 = new Post("Zuri Nuestra bella princesa ya esta lista para dejar el refu y llenar de amor a una familia, tamaño de mediano a grande, se entrega castrada y con contrato de adopción!\n" + 
+				"Veni a conocer a ZURI en nuestra próxima jornada de visita!",Encoder.encode(dogs.get(3), "jpg"),0,0,"Varela",Category.ADOPCION);
 		
 		postDAO.save(post4);
 		postDAO.save(post5);
