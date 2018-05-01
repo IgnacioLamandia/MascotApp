08d9f4a57f4e4be07ea7dc913e7a1492fecf5e8c
jeretome <you@example.com>
2018-04-30 22:58:45 -0300

    Agregada clase con la configuración provista por Google Firebase

---
+++ mascotapp-frontend/src/config.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class Config {
+}
+
+export const firebaseConfig = {
+	fire: {
+    apiKey: "AIzaSyDqLfsxAjugZ4q3TC1bUlipCvEn_Fs8P4w",
+    authDomain: "mascotapp-540df.firebaseapp.com",
+    databaseURL: "https://mascotapp-540df.firebaseio.com",
+    projectId: "mascotapp-540df",
+    storageBucket: "mascotapp-540df.appspot.com",
+    messagingSenderId: "739412183808"
+	}
+};
